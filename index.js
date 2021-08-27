260c047e1991856a463d69cd7c703399e649c203
Junai <juniormaidana36@gmail.com>
2021-08-27 16:55:56 -0300

    Fixed message_create

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-// index.js
-// For help, go to the repositorie on GitHub
-// 
+/*  index.js
+
+ For help, go to the repositorie on GitHub
+
+*/ 
 
 // Packages
 require('dotenv').config()
@@ -8,16 +10,13 @@ const fs = require('fs');
 const qrcode = require('qrcode-terminal');
 const { Client } = require('whatsapp-web.js');
 
-// Json
+// Config
 const config = require("./config.json")
-
-// Initialize code
-const SESSION_FILE_PATH = "./session.json";
-
-// Number configuration
 const country_code = config.country_code;
 const number = config.number;
 
+// Initialize code
+const SESSION_FILE_PATH = "./session.json";
 let sessionData;
 
 if(fs.existsSync(SESSION_FILE_PATH)) {
@@ -57,8 +56,9 @@ client.on('ready', () => {
 })
 
 
-// Commands
-// Help
+/* Commands
+    Help
+*/
 client.on('message', msg => {
     // Help
     if(msg.body == "/help") {
@@ -69,61 +69,28 @@ client.on('message', msg => {
     }
 
     // 8 Ball
-    else if(msg.body.startsWith("/8ball")) {
-        if(msg.body.slice(7) === null) {
+    else if(msg.body.startsWith("/8ball ")) {
+        if(msg.body.slice(7) == null) {
             client.sendMessage(msg.from, "That is even a question?");
-        }
+        } else {
+            const random_number = Math.floor(Math.random() * 11) + 0;
+            const text = [  
+            "Yes!", 
+            "No! :(", 
+            "May be", 
+            "Probably", 
+            "Probably not",
+            "Uhh, I'm hungry, Im going to the kitchen", 
+            "Everything points to yes", 
+            "Everything points to no ",
+            "Clearly",
+            "You must trust it",
+            "Very doubtful",
+            "Not Very doubtful"
+            ] 
 
-        else{
-            var random_number = Math.floor(Math.random() * 12) + 1;
-        
-            if(random_number == 1){
-                msg.reply("Yes!");
-            }
+            msg.reply(text[random_number])
 
-            if(random_number == 2){
-                msg.reply("No! :(");
-            }
-
-            if(random_number == 3){
-                msg.reply("May be");
-            }
-
-            if(random_number == 4){
-                msg.reply("Probably");
-            }
-
-            if(random_number == 5){
-                msg.reply("Probably not");
-            }
-
-            if(random_number == 6){
-                msg.reply("Uhh, I'm hungry, Im going to the kitchen");
-            }
-
-            if(random_number == 7){
-                msg.reply("Everything points to yes");
-            }
-
-            if(random_number == 8){
-                msg.reply("Everything points to no");
-            }
-
-            if(random_number == 9){
-                msg.reply("Clearly");
-            }
-
-            if(random_number == 10){
-                msg.reply("You must trust it");
-            }
-
-            if(random_number == 11){
-                msg.reply("Very doubtful");
-            }
-
-            if(random_number == 12){
-                msg.reply("Very doubtful");
-            }
         }
     }
 })
